b6ab11efe17b3a9e42ef465afe785cd59e26dcf8
Travis Zhang <wr.zhang25@gmail.com>
2026-01-28 16:18:42 +0800

    chore: daily development

--- tests/baseTest.ts
+++ tests/baseTest.ts
@@ -5,7 +5,6 @@ export const test = baseTest.extend({
     context: createCoverageContextFixture({
         outputDir:'.canyon_output'
     }),
-    headless:false
 });
 
 export const expect = test.expect;
